70c453357ab6765786f1169f01fbf00ea7bd656c
ZanSara <sarazanzo94@gmail.com>
2023-03-29 02:06:49 +0200

    move the loop into python

--- convert_issue_to_pr.py
+++ convert_issue_to_pr.py
@@ -1,10 +1,18 @@
 import os
 import sys
 import json
+import subprocess
 import openai
 
+SYSTEM_PROMPT = """
+You are a code assistant. You will be given a codebase and an issue. 
+You should reply with a patch that will fix the issue.
+Do not describe your output. Only output the git patch that fixes the issue. 
+The output will be piped to a file and applied to the repository, so make sure the syntax is valid.
+"""
 
-def issue_to_pr(codebase, feedback, issue_content):
+
+def issue_to_pr(codebase, issue_content):
     issue_data = json.loads(issue_content)
 
     codebase_content = ""
@@ -14,29 +22,33 @@ def issue_to_pr(codebase, feedback, issue_content):
             with open(codebase + "/" + file_to_load, 'r') as code_file:
                 codebase_content += f"```\n{code_file.read()}\n```\n\n"
 
-    response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-            {
-                "role": "system", 
-                "content": """
-                    You are a code assistant. You will be given a codebase and an issue. 
-                    You should reply with a patch that will fix the issue.
-                    Do not describe your output. Only output the git patch that fixes the issue. 
-                    The output will be piped to a file and applied to the repository, so make sure the syntax is valid.
-                """
-            },
-            {
-                "role": "user", 
-                "content": f"CODEBASE:\n\n{codebase_content}\n\nISSUE:\n\n{issue_data}\n\nFEEDBACK:{feedback}\n\nPATCH:"
-            }
-        ]
-    )
-    reply = response["choices"][0]["message"]["content"]
+    prompt = f"CODEBASE:\n\n{codebase_content}\n\nISSUE:\n\n{issue_data}\n\nPATCH:"
+
+    for i in range(10):
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+                {"role": "system",  "content": SYSTEM_PROMPT},
+                {"role": "user", "content": prompt}
+            ]
+        )
+        reply = response["choices"][0]["message"]["content"]
+        
+        with open("changes.patch", "w") as patch_file:
+            patch_file.write(reply)
+
+        apply_patch="git apply --ignore-space-change --ignore-whitespace changes.patch"
+        try:
+            apply_command = subprocess.run(apply_patch, shell=True, check=True)
+            break
+        except:
+            pass
+        print("FAILED!")
+
     reply = reply.replace('"', "\"")  # Bash
     return reply
 
 
 if __name__ == "__main__":
     openai.api_key = sys.argv[1]
-    print(issue_to_pr(issue_content=sys.argv[2], feedback=sys.argv[3], codebase=sys.argv[4]))
+    print(issue_to_pr(issue_content=sys.argv[2], codebase=sys.argv[3]))
